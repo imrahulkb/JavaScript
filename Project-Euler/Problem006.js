19b8ad851d3a8a617f95e119bfad84bd7dac88c7
Rahul Bhandari <90493221+imrahulkb@users.noreply.github.com>
2023-10-14 19:48:23 +0530

    Update Problem006.js

--- Project-Euler/Problem006.js
+++ Project-Euler/Problem006.js
@@ -1,11 +1,8 @@
 // https://projecteuler.net/problem=6
 
 export const squareDifference = (num = 100) => {
-  let sumOfSquares = 0
-  let sums = 0
-  for (let i = 1; i <= num; i++) {
-    sumOfSquares += i ** 2 // add squares to the sum of squares
-    sums += i // add number to sum to square later
-  }
+   let sumOfSquares = (100)*(100+1)*(200+1)/6
+  let sums = (100*101)/2
+  
   return sums ** 2 - sumOfSquares // difference of square of the total sum and sum of squares
 }
